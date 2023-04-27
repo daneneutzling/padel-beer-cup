84a5509d9c7d1f7cdeeb798889e482f6c2a1f42a
Daniela Neutzling <daneneutzling@gmail.com>
2023-04-27 14:39:55 -0300

    Add Chakra

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
 
 function App() {
   return (
-    <div>
-      TESTE DE PROJETO
-    </div>
+    <ChakraProvider>
+    </ChakraProvider>
   );
 }
 
