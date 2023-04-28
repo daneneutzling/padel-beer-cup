a0e3d70c11b0f63e8521291c48178decae2ff82b
Daniela Neutzling <daneneutzling@gmail.com>
2023-04-28 08:39:59 -0300

    Add User Icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import { ChakraProvider } from "@chakra-ui/react"
-import { Button } from "@chakra-ui/react"
+import { Avatar } from "@chakra-ui/react";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider className='chackra'>
+      <div className='App'>
+        <div className='icon-user'>
+          <Avatar bg='teal.500'/>
+        </div>
+      </div>
     </ChakraProvider>
   );
 }
