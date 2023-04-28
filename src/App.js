db02ddc88107866ab46b6e5e9960ca88f3211394
Daniela Neutzling <daneneutzling@gmail.com>
2023-04-28 08:42:31 -0300

    Add Input User

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { ChakraProvider } from "@chakra-ui/react"
-import { Avatar } from "@chakra-ui/react";
+import { Input, Avatar, InputGroup, InputLeftAddon } from "@chakra-ui/react";
 
 function App() {
   return (
@@ -8,6 +8,13 @@ function App() {
       <div className='App'>
         <div className='icon-user'>
           <Avatar bg='teal.500'/>
+        </div>
+
+        <div className='input-group'>
+          <InputGroup>
+            <InputLeftAddon children='User'/>
+            <Input type='text'/>
+          </InputGroup>
         </div>
       </div>
     </ChakraProvider>
