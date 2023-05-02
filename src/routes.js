573ad8ee27ee9bed8e8472ea0f66ccc3e4f6887b
Daniela Neutzling <daneneutzling@gmail.com>
2023-05-01 22:48:27 -0300

    Do Home.js route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login";
+import Home from "./Home";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />}></Route>
+                <Route path="/home" element={<Home />}></Route>
             </Routes>
         </BrowserRouter>
     );
