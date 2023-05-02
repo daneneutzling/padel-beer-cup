0557b651694971eaa59c8e2fafb17d048eae4a26
Daniela Neutzling <daneneutzling@gmail.com>
2023-05-01 22:39:47 -0300

    Add routes.js

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    );
+}
+
+export default AppRoutes;
