44c584d6008d515ae323dd79d3dd3bcf52fcb588
Daniela Neutzling <daneneutzling@gmail.com>
2023-05-09 11:31:37 -0300

    Add more temporary links

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { Route, BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Box, Flex, Heading, Link, Text } from "@chakra-ui/react";
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons';
+import { Box, Flex, Heading, Link, Text, Divider } from "@chakra-ui/react";
+import { CheckCircleIcon, LinkIcon, InfoIcon } from '@chakra-ui/icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faFacebook, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelopeOpen } from "@fortawesome/free-regular-svg-icons";
 
 function Home() {
 
@@ -19,15 +22,34 @@ function Home() {
         </Box>
 
         <Link href='/Home' _hover={{textDecoration:"none"}}>
+          <Box w="90%" h={9} m={2} mt="15%" bg="teal" borderRadius="md" display="flex" alignItems="center" p="4%" >
+            <LinkIcon ml="8%"/>
+            <Text display="flex" justifyContent="center" ml="15%">Link Link</Text>
+          </Box>
+        </Link>
+
+        <Link href='/Home' _hover={{textDecoration:"none"}}>
           <Box w="90%" h={9} m={2} bg="teal" borderRadius="md" display="flex" alignItems="center" p="4%" >
             <LinkIcon ml="8%"/>
             <Text display="flex" justifyContent="center" ml="15%">Link Link</Text>
           </Box>
         </Link>
 
+        <Divider borderColor="gray.400" borderWidth="1px" w="99%"/>
+
+        <Link href='/Home' _hover={{textDecoration:"none"}} mt="10%">
+          <Box w="90%" h={9} m={2} mt="15%" bg="teal" borderRadius="md" display="flex" alignItems="center" p="4%" >
+            <InfoIcon ml="8%"/>
+            <Text display="flex" justifyContent="center" ml="15%">Link Link</Text>
+          </Box>
+        </Link>
       </Box>
     </ChakraProvider>
   );
 }
 
 export default Home;
+
+
+
+
