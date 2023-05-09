2e6a8822f290bdcb969403154f63febf1ee7db01
Daniela Neutzling <daneneutzling@gmail.com>
2023-05-09 09:52:54 -0300

    Add logo and link temporary/test

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
 import { Route, BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Stack, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Link, Text } from "@chakra-ui/react";
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons';
 
 function Home() {
 
   return (
     <ChakraProvider>
-      <Flex justify="center">
-        <Stack w={500} border="1px solid #f5f5f5" p={2} mt="10%">
-            <Heading>Welcome to Page Home</Heading>
-        </Stack>
-      </Flex>
+      <Box maxW={200} w={200} h="100%" bg="gray.200" position="fixed">
+        <Box m={3}>
+          <a href='/Home'>
+            <Flex>
+              <CheckCircleIcon w={8} h={8}/>
+              <Heading ml={3} mt={2} size='sm'>Padel Beer Cup</Heading>
+            </Flex>
+          </a>
+        </Box>
+
+        <Link href='/Home' _hover={{textDecoration:"none"}}>
+          <Box w="90%" h={9} m={2} bg="teal" borderRadius="md" display="flex" alignItems="center" p="4%" >
+            <LinkIcon ml="8%"/>
+            <Text display="flex" justifyContent="center" ml="15%">Link Link</Text>
+          </Box>
+        </Link>
+
+      </Box>
     </ChakraProvider>
   );
 }
